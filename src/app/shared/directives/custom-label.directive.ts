0d1f168df670c9dba50b7365dfc93f84ab1787df
Ezequiel Lino <sonisombra@gmail.com>
2023-11-14 11:10:16 -0600

    cambiar el mensaje de la etiqueta

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]'
@@ -7,29 +8,47 @@ export class CustomLabelDirective implements OnInit{
 
   private htmlElement?: ElementRef<HTMLElement>;
   private _color:string = 'red';
+  private _errors?: ValidationErrors | null;
 
   @Input() set color( value:string ) {
     this._color = value;    
     this.setStyle()
   }
 
+  @Input() set errors( value: ValidationErrors | null | undefined ) {
+    this._errors = value;
+    this.setErrorsMessage()
+  }
+
   constructor( private el: ElementRef<HTMLElement>) { 
     // console.log('constructor de la directiva');
-    console.log(el)
+    // console.log(el)
     this.htmlElement = el;
   }
 
   ngOnInit(): void {
-    console.log('directiva - ngoninit');
-
     this.setStyle();
   }
 
   setStyle():void {
     if( !this.htmlElement ) return;
 
-    this.htmlElement!.nativeElement.style.color = this._color
+    this.htmlElement!.nativeElement.style.color = this._color;
   }
 
+  setErrorsMessage():void{
+    if( !this.htmlElement ) return;
+    if( !this._errors ) {
+      this.htmlElement!.nativeElement.innerText = '';
+      return;
+    }
 
+    const errors = Object.keys(this._errors);
+    console.log(errors)
+
+    if( errors.includes('required') ) {
+      this.htmlElement.nativeElement.innerText = "Campo requerido";
+      return;
+    }
+  }
 }

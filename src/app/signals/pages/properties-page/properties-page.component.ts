eeaf17364ee2fca52a9d397dd42f42081dd2e182
Ezequiel Lino <sonisombra@gmail.com>
2023-11-23 10:54:34 -0600

    diseño de la estructura

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-properties-page',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./properties-page.component.css']
 })
 export class PropertiesPageComponent {
+
+  public user = signal<User>({
+    id: 7,
+    email: 'michael.lawson@reqres.in',
+    first_name: 'Michael',
+    last_name: 'Lawson',
+    avatar: 'https://reqres.in/img/faces/7-image.jpg'
+  })
+
+  onFieldUpdated(field: string, value: string){
+    console.log(field, value);
+    
+  }
 
 }

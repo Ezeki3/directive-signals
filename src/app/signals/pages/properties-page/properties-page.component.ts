998728b7b4880d55bed9acb0ec95177b6a49e374
Ezequiel Lino <sonisombra@gmail.com>
2023-11-23 11:38:15 -0600

    mutaciones- actualizar parte del estado

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -16,8 +16,36 @@ export class PropertiesPageComponent {
     avatar: 'https://reqres.in/img/faces/7-image.jpg'
   })
 
-  onFieldUpdated(field: string, value: string){
+  onFieldUpdated(field: keyof User, value: string){
     console.log(field, value);
+
+    this.user.update( current => {
+
+      switch( field ) {
+
+        case 'email':
+          current.email = value;
+          break;
+
+        case 'avatar':
+        current.avatar = value;
+        break;
+
+        case 'first_name':
+          current.first_name = value;
+          break;
+
+        case 'last_name':
+          current.last_name = value;
+          break;
+
+        case 'id':
+        current.id = Number(value);
+          break;
+      }
+
+      return current;
+    })
     
   }
 
